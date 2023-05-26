adefe71bafb6d1408287ee207d352d36b19b38a7
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-05-26 16:04:20 +0200

    error handling

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import Errorhandler from "./utils/errorHandler.js";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config();
 }
+
+// error hanler middleware
+app.use(Errorhandler);
 
 export default app;
