9d213732f88faf90af42dad76fb262b626c7a30e
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-05-26 23:08:01 +0200

    user model

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ import dotenv from "dotenv";
 import Errorhandler from "./utils/errorHandler.js";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import fileUpload from "express-fileupload";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({ useTempFiles: true }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
---
+++ models/User.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email!"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [4, "Password should be greater than 4 characters"],
+    select: false,
+  },
+  phoneNumber: {
+    type: Number,
+  },
+  addresses: [
+    {
+      country: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      address1: {
+        type: String,
+      },
+      address2: {
+        type: String,
+      },
+      zipCode: {
+        type: Number,
+      },
+      addressType: {
+        type: String,
+      },
+    },
+  ],
+  role: {
+    type: String,
+    default: "user",
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetPasswordToken: String,
+  resetPasswordTime: Date,
+});
+
+// hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// jwt
+userSchema.methods.getJWT = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
