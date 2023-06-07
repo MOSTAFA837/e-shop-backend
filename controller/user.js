83aabc9a851b1c9c0884d7d47e9d9fb5c35d1311
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-06-07 20:11:35 +0200

    send an email to signup user

--- controller/user.js
+++ controller/user.js
@@ -1,9 +1,12 @@
 import express from "express";
 import fs from "fs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/User.js";
 import upload from "../multer.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import path from "path";
-import User from "../models/User.js";
+import { sendMail } from "../utils/sendMail.js";
 
 const router = express.Router();
 
@@ -41,15 +44,31 @@ export const signup = router.post(
         avatar: fileUrl,
       };
 
-      // console.log(user);
-      const newUser = await User.create(user);
+      const activationToken = createActivationToken(user);
+      const activationUrl = `${process.env.CLIENT_URL}/activation/${activationToken}`;
 
-      res.status(201).json({
-        success: true,
-        newUser,
-      });
+      try {
+        await sendMail({
+          email: user.email,
+          subject: "Activte your account",
+          message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+        });
+
+        res.status(201).json({
+          success: true,
+          message: `Please check your email address: ${user.email} to activate your account`,
+        });
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
     } catch (error) {
       return next(new ErrorHandler(error.message, 400));
     }
   }
 );
+
+const createActivationToken = (user) => {
+  return jwt.sign(user, `${process.env.ACTIVATION_SECRET}`, {
+    expiresIn: "5m",
+  });
+};
